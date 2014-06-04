d57c45689f1b7f688e2391e90661fa823c3166f0
taylorchiu <taylorparkerchiu@gmail.com>
2014-06-04 14:17:14 -0700

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,13 @@
+class BooksController < ApplicationController
+
+	respond_to :json
+	
+	def index
+		respond_with Book.all
+	end
+
+	def show
+		respond_with Book.find(params[:id])
+	end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :books
+end
+
+
+#    Prefix Verb   URI Pattern               Controller#Action
+#     books GET    /books(.:format)          books#index
+#           POST   /books(.:format)          books#create
+#  new_book GET    /books/new(.:format)      books#new
+# edit_book GET    /books/:id/edit(.:format) books#edit
+#      book GET    /books/:id(.:format)      books#show
+#           PATCH  /books/:id(.:format)      books#update
+#           PUT    /books/:id(.:format)      books#update
+#           DELETE /books/:id(.:format)      books#destroy
